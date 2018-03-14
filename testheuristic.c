7c7aae116c842c8ddcc1e15d95f0b5dbf8f7fb7f
AdamHayse <ahayse501@gmail.com>
2018-03-14 09:31:49 -0400

    Added an ASCII graphic for better visualization of the cube

--- testheuristic.c
+++ testheuristic.c
@@ -19,6 +19,7 @@
 static uint8_t cdatabase[C_DB_SIZE];
 static uint8_t e1database[E_DB_SIZE];
 static uint8_t e2database[E_DB_SIZE];
+void display_cube(uint8_t *corners, uint8_t *edges);
 
 int main() {
 
@@ -141,25 +142,69 @@ int main() {
       token = strtok(NULL, " \n()[]\t{}");
     }
 
-    // Print resulting combination.
-
-    int k;
-    for (k=0; k<8; k++)
-      printf("%2u, ", corners[i][k]);
-    putchar('\n');
-    for (k=0; k<12; k++)
-      printf("%2u, ", edges[i][k]);
-    putchar('\n');
-
-    // Lookup minimum moves from loaded pattern databases.
-    printf("%u moves to solve corners.\n", C_path_length(corners[i], cdatabase));
-    printf("%u moves to solve edges 1-6.\n", E1_path_length(edges[i], e1database));
-    printf("%u moves to solve edges 7-12.\n", E2_path_length(edges[i], e2database));
+    // Print resulting combination and database results.
+    display_cube(corners[i], edges[i]);
   }
 }
 
+void display_cube(uint8_t *corners, uint8_t *edges) {
 
+  // Set up cubies.
+  char c[8][3][4] = {"YOG", "GYO", "OGY",
+                     "YGR", "RYG", "GRY",
+                     "YRB", "BYR", "RBY",
+                     "YBO", "OYB", "BOY",
+                     "WGO", "OWG", "GOW",
+                     "WRG", "GWR", "RGW",
+                     "WBR", "RWB", "BRW",
+                     "WOB", "BWO", "OBW"};
+  char e[12][2][3] = {"YO", "OY",
+                      "YG", "GY",
+                      "YR", "RY",
+                      "YB", "BY",
+                      "OG", "GO",
+                      "GR", "RG",
+                      "RB", "BR",
+                      "BO", "OB",
+                      "WO", "OW",
+                      "WG", "GW",
+                      "WR", "RW",
+                      "WB", "BW"};
 
+  // Assign faces.
+  char face[54];
+  int i, j;
+  for (i=0; i<8; i++)
+    for (j=0; j<3; j++) 
+      face[3*i+j] = c[corners[i]/3][corners[i]%3][j];
+  for (i=0; i<12; i++)
+    for (j=0; j<2; j++) 
+      face[24+2*i+j] = e[edges[i]/2][edges[i]%2][j];
 
-
-
+  // Print cube.
+  printf("\n               -----------\n");
+  printf("              | %c | %c | %c |\n",face[3],face[28],face[6]);
+  printf("               -----------\n");
+  printf("              | %c | Y | %c |\n",face[26],face[30]);
+  printf("               -----------\n");
+  printf("              | %c | %c | %c |\n",face[0],face[24],face[9]);
+  printf("  -----------  ===========  -----------  -----------\n");
+  printf(" | %c | %c | %c || %c | %c | %c || %c | %c | %c || %c | %c | %c |\n",face[4],face[27],
+  face[2],face[1],face[25],face[11],face[10],face[31],face[8],face[7],face[29],face[5]);
+  printf("  -----------  -----------  -----------  -----------\n");
+  printf(" | %c | G | %c || %c | O | %c || %c | B | %c || %c | R | %c |\n",
+  face[34],face[33],face[32],face[39],face[38],face[37],face[36],face[35]);
+  printf("  -----------  -----------  -----------  -----------\n");
+  printf(" | %c | %c | %c || %c | %c | %c || %c | %c | %c || %c | %c | %c |\n",face[17],face[43],
+  face[13],face[14],face[41],face[22],face[23],face[47],face[19],face[20],face[45],face[16]);
+  printf("  -----------  ===========  -----------  -----------\n");
+  printf("              | %c | %c | %c |\n",face[12],face[40],face[21]);
+  printf("               -----------\n");
+  printf("              | %c | W | %c |     ",face[42],face[46]);
+  printf("%u moves to solve corners.\n",C_path_length(corners, cdatabase));
+  printf("               -----------      ");
+  printf("%u moves to solve edges 1-6.\n",E1_path_length(edges, e1database));
+  printf("              | %c | %c | %c |     ",face[15],face[44],face[18]);
+  printf("%u moves to solve edges 7-12.\n",E2_path_length(edges, e2database));
+  printf("               -----------\n\n");
+}
