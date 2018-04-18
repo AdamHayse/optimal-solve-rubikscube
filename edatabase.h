8e344eed95df6e0313ac721a356759c859ce901f
AdamHayse <ahayse501@gmail.com>
2018-04-17 21:57:39 -0400

    Added ability to generate many size edge databases

--- edatabase.h
+++ edatabase.h
@@ -19,7 +19,27 @@
   #ifndef TRACKED_EDGES
     #define TRACKED_EDGES 6
   #endif
-  #if TRACKED_EDGES == 6
+  #if TRACKED_EDGES == 1
+    #define TRACKED_NAME "1"
+    #define BOUND 1
+    #define E_DB_SIZE 12UL     // 12!/11! x 2^1 / 2
+  #elif TRACKED_EDGES == 2
+    #define TRACKED_NAME "3"
+    #define BOUND 3
+    #define E_DB_SIZE 264UL     // 12!/10! x 2^2 / 2
+  #elif TRACKED_EDGES == 3
+    #define TRACKED_NAME "3"
+    #define BOUND 5
+    #define E_DB_SIZE 5280UL     // 12!/9! x 2^3 / 2
+  #elif TRACKED_EDGES == 4
+    #define TRACKED_NAME "4"
+    #define BOUND 7
+    #define E_DB_SIZE 95040UL     // 12!/8! x 2^4 / 2
+  #elif TRACKED_EDGES == 5
+    #define TRACKED_NAME "5"
+    #define BOUND 9
+    #define E_DB_SIZE 1520640UL     // 12!/7! x 2^5 / 2
+  #elif TRACKED_EDGES == 6
     #define TRACKED_NAME "6"
     #define BOUND 11
     #define E_DB_SIZE 21288960UL     // 12!/6! x 2^6 / 2
@@ -31,6 +51,10 @@
     #define TRACKED_NAME "8"
     #define BOUND 15
     #define E_DB_SIZE 2554675200ULL   // 12!/4! x 2^8 / 2
+  #elif TRACKED_EDGES == 9
+    #define TRACKED_NAME "9"
+    #define BOUND 17
+    #define E_DB_SIZE 20437401600ULL   // 12!/3! x 2^9 / 2
   #else
     #error "TRACKED_EDGES must be 6, 7, or 8"
   #endif
@@ -42,7 +66,27 @@
   #ifndef TRACKED_EDGES
     #define TRACKED_EDGES 6
   #endif
-  #if TRACKED_EDGES == 6
+  #if TRACKED_EDGES == 1
+    #define TRACKED_NAME "1"
+    #define BOUND 22
+    #define E_DB_SIZE 12UL     // 12!/11! x 2^1 / 2
+  #elif TRACKED_EDGES == 2
+    #define TRACKED_NAME "3"
+    #define BOUND 20
+    #define E_DB_SIZE 264UL     // 12!/10! x 2^2 / 2
+  #elif TRACKED_EDGES == 3
+    #define TRACKED_NAME "3"
+    #define BOUND 18
+    #define E_DB_SIZE 5280UL     // 12!/9! x 2^3 / 2
+  #elif TRACKED_EDGES == 4
+    #define TRACKED_NAME "4"
+    #define BOUND 16
+    #define E_DB_SIZE 95040UL     // 12!/8! x 2^4 / 2
+  #elif TRACKED_EDGES == 5
+    #define TRACKED_NAME "5"
+    #define BOUND 14
+    #define E_DB_SIZE 1520640UL     // 12!/7! x 2^5 / 2
+  #elif TRACKED_EDGES == 6
     #define TRACKED_NAME "6"
     #define BOUND 12
     #define E_DB_SIZE 21288960UL     // 12!/6! x 2^6 / 2
@@ -54,6 +98,10 @@
     #define TRACKED_NAME "8"
     #define BOUND 8
     #define E_DB_SIZE 2554675200ULL   // 12!/4! x 2^8 / 2
+  #elif TRACKED_EDGES == 9
+    #define TRACKED_NAME "9"
+    #define BOUND 6
+    #define E_DB_SIZE 20437401600ULL   // 12!/3! x 2^9 / 2
   #else
     #error "TRACKED_EDGES must be 6, 7, or 8"
   #endif
